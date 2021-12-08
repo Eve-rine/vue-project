6203e9c4612644578e09f78d00809a4c2fd5a065
Evrine <minayoevrine@gmail.com>
2021-12-08 10:20:02 +0300

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <v-alert
+  type="success"
+> This is an alert</v-alert>
+  <v-alert
+ dense
+      type="info"    
+    >
+      I'm a dense alert with the <strong>text</strong> prop and a <strong>type</strong> of success
+    </v-alert>
+  </div>
+</template>
